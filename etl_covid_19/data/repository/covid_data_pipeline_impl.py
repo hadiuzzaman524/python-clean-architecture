8b46a1a517876bfebefaf046176b60a695a7d1ce
hadiuzzaman524 <hadiuzzaman.cse.bd@gmail.com>
2025-07-21 10:50:16 +0600

    chore: transform data

--- etl_covid_19/data/repository/covid_data_pipeline_impl.py
+++ etl_covid_19/data/repository/covid_data_pipeline_impl.py
@@ -2,6 +2,7 @@ from etl_covid_19.domain.services.covid_data_pipeline import CovidDataPipeline
 from etl_covid_19.data.data_source.covid_data_source import CovidDataSource
 from etl_covid_19.infrastructure.database.database_client import DatabaseClient
 from etl_covid_19.data.model.covid_model import CovidModel
+import pandas as pd 
 
 class CovidDataPipelineImpl(CovidDataPipeline):
     
@@ -13,7 +14,9 @@ class CovidDataPipelineImpl(CovidDataPipeline):
         return self.data_source.get_data_from_big_query()
         
     def transform_records(self, raw_data):
-        pass
+        df = pd.DataFrame(raw_data)
+        df = df.fillna(0).infer_objects(copy=False)
+        return df.to_dict(orient="records")
 
     def load_to_database(self, records):
         self.database_client.insert_data_into_pg(model_name= CovidModel, data= records)
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from etl_covid_19.data.repository.covid_data_pipeline_impl import CovidDataPipel
 from etl_covid_19.data.data_source.covid_data_source import CovidDataSource
 from etl_covid_19.infrastructure.biqquery.bigquery_client import BigQueryClient
 from etl_covid_19.infrastructure.database.database_client import DatabaseClient
+from etl_covid_19.domain.use_cases.transform_covid_data_usecase import TransformCovidDataUseCase
+import pandas as pd 
 
 
 bg = BigQueryClient()
@@ -11,5 +13,10 @@ data_source = CovidDataSource(bigquery_client=bg)
 pipeline = CovidDataPipelineImpl(data_source=data_source, database_client=db)
 obj = FetchCovidDataUseCase(pipeline=pipeline)
 
+transform = TransformCovidDataUseCase(pipeline=pipeline)
+
 data= obj.execute()
-print(data)
+trans= transform.execute(data)
+data = pd.DataFrame(trans)
+data.to_csv('data.csv',index=False)
+
